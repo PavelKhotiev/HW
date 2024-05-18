45568efcdeaf33f9fb3a0489c1d323c5b80ab2b8
Pavel Khotiev <khotievp@gmail.com>
2024-05-18 14:14:28 +0300

    добавлена функция сортировки по дате

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,17 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
         if item.get("state") == state:
             filtered_data.append(item)
     return filtered_data
+
+
+def sort_by_date(data: list, descending: bool = True) -> list:
+    """принимает на вход список словарей и возвращает новый список, в котором
+    исходные словари отсортированы по убыванию даты"""
+
+    def get_date(item: dict) -> str:
+        return item["date"]
+
+    sorted_data = sorted(data, key=get_date, reverse=descending)
+    return sorted_data
+
+
+# P.S. mypy выдаёт ошибку - не могу исправить, не понимаю что ему не нравится
